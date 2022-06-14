fb19cef3a6a751a5d5e0ba968524db081ca34a45
jimlin317 <103529873+jimlin317@users.noreply.github.com>
2022-06-15 06:51:59 +0800

    Add files via upload

---
+++ 今天晚餐吃什麼/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("input").on("click",function(){
+      var numberOfListItem = $("li").length;
+      var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
+      $("h1").text($("li").eq(randomChildNumber).text());
+    });
+});
